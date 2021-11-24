a1a7f37cd7d922818ff928d12ead95098f11c01a
vika <viktoriia.kulyk@privatbank.ua>
2021-11-24 18:42:38 +0200

    fix response body logging

--- src/main/java/com/kiberohrannik/webflux_addons/logging/response/filter/LogResponseFilter.java
+++ src/main/java/com/kiberohrannik/webflux_addons/logging/response/filter/LogResponseFilter.java
@@ -1,8 +1,30 @@
 package com.kiberohrannik.webflux_addons.logging.response.filter;
 
+import com.kiberohrannik.webflux_addons.logging.request.message.RequestBodyExtractor;
+import com.kiberohrannik.webflux_addons.logging.response.message.ResponseData;
+import com.kiberohrannik.webflux_addons.logging.response.message.ResponseMessageCreator;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
+import reactor.core.publisher.Mono;
 
-public interface LogResponseFilter {
+@RequiredArgsConstructor
+public class LogResponseFilter implements ExchangeFilterFunction {
 
-    ExchangeFilterFunction logResponse();
+    private static final Log log = LogFactory.getLog(RequestBodyExtractor.class);
+    private final ResponseMessageCreator messageCreator;
+
+
+    @Override
+    public Mono<ClientResponse> filter(ClientRequest request, ExchangeFunction next) {
+        return next.exchange(request)
+                .elapsed()
+                .flatMap(tuple -> messageCreator.formatMessage(tuple.getT1(), tuple.getT2())
+                        .doOnNext(responseData -> log.info(responseData.getLogMessage()))
+                        .map(ResponseData::getResponse));
+    }
 }
